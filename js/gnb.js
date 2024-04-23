f5bebb6fdd785ea6d145d0a0737927f967423826
pingandthepong <pingandthepong@gmail.com>
2024-04-23 20:44:38 +0900

    index #gnb .depth1 스타일링

--- js/gnb.js
+++ js/gnb.js
@@ -1,22 +1,21 @@
 $(document).ready(function () {
-  var op = false; //네비가 열려있으면(true) , 닫혀있으면(false)
+  var op = false; // nav open = true , close = false
 
   $(".menu_ham, .modal").click(function (e) {
-    //메뉴버튼 클릭시
     e.preventDefault();
 
-    var documentHeight = $(document).height(); //전체 페이지의 높이
-    $("#gnb").css("height", documentHeight); //네비의 높이를 전체 전체 페이지의 높이로...
+    var documentHeight = $(document).height();
+    $("#gnb").css("height", documentHeight);
 
     if (op == false) {
-      //메뉴가 닫혀있는 상태에서 클릭했냐??
-      $("#gnb").animate({ left: 0, opacity: 1 }, "fast");
+      // 메뉴가 닫혀있는 상태에서 클릭 시
+      $("#gnb").animate({ right: 0, opacity: 1 }, "fast");
       $("#headerArea").addClass("mn_open");
       $("#headerArea .modal").fadeIn("fast");
       op = true;
     } else {
-      //메뉴가 열려있는 상태에서 클릭했냐??
-      $("#gnb").animate({ left: "-100%", opacity: 0 }, "fast");
+      // 메뉴가 열려있는 상태에서 클릭 시
+      $("#gnb").animate({ right: "-100%", opacity: 0 }, "fast");
       $("#headerArea").removeClass("mn_open");
       $("#headerArea .modal").fadeOut("fast");
       op = false;
@@ -41,6 +40,7 @@ $(document).ready(function () {
     if (onoff[ind] == false) {
       //각각의 1depth메뉴의 서브가 닫혀있냐??
       //$('#gnb .depth1 ul').hide();
+      $(this).addClass('on');
       $(this).parents(".depth1").find("ul").slideDown("slow"); //자신의 서브를 열어라
       $(this).parents(".depth1").siblings("li").find("ul").slideUp("fast"); //자신을 제외한 모든 서브를 닫아라
       for (var i = 0; i < arrcount; i++) onoff[i] = false; //모든 배열값을 false
@@ -50,6 +50,7 @@ $(document).ready(function () {
       $(this).find("span").text("-");
     } else {
       //각각의 1depth메뉴의 서브가 열려있냐??
+      $(this).removeClass('on');
       $(this).parents(".depth1").find("ul").slideUp("fast"); //지 서브를 닫아라~~
       onoff[ind] = false; // true->false
 
