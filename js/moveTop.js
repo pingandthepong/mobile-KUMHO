4ffb891edd22826bafa692a5fc29850451f88a2f
pingandthepong <pingandthepong@gmail.com>
2024-04-25 16:58:08 +0900

    footer .move_top requestAnimationFrame 방식 (여태까지 안됐던 이유는.. class명에 오탈자 있었음)

--- js/moveTop.js
+++ js/moveTop.js
@@ -1,46 +1,23 @@
-// function moveTop() {
-//   var scroll = $(window).scrollTop();
+var lastScrollTopFooter = 0;
+var moveTopVisible = false;
 
-//   if(scroll > 300) {
-//     $('.moveTop').stop().animate({opacity: 1}, 100);
-    
-//   } else if (scroll <= 300) {
-//     $('.moveTop').stop().animate({opacity: 0}, 100);
-    
-//   }
+function moveTop() {
+  var scroll = $(window).scrollTop();
 
-//   lastScrollTopFooter = scroll;
-// };
-// var lastScrollTopFooter = 0;
-// var moveTopVisible = false;
-
-// function moveTop() {
-//   var scroll = $(window).scrollTop();
-
-//   if(scroll > lastScrollTopFooter && scroll > 300 && !moveTopVisible) {
-//     $('.moveTop').stop().animate({opacity: 1}, 100);
-//     moveTopVisible = true;
-//   } else if (scroll <= 300 && moveTopVisible) {
-//     $('.moveTop').stop().animate({opacity: 0}, 100);
-//     moveTopVisible = false;
-//   }
-
-//   lastScrollTopFooter = scroll;
-// };
-
-// $(window).on('scroll', function() {
-//   requestAnimationFrame(moveTop);
-// });
-
-$(window).on('scroll',function(){
-  var windowTop = $(window).scrollTop();
-  if(windowTop > 500){
-      $('.move_top').css({opacity: 1});
-  }else{
-      $('.move_top').css({opacity: 0});
+  if(scroll > lastScrollTopFooter && scroll > 500 && !moveTopVisible) {
+    $('.move_top').stop().animate({opacity: 1}, 100);
+    moveTopVisible = true;
+  } else if (scroll <= 500 && moveTopVisible) {
+    $('.move_top').stop().animate({opacity: 0}, 100);
+    moveTopVisible = false;
   }
-});
+  
+  lastScrollTopFooter = scroll;
+};
 
+$(window).on('scroll', function() {
+  requestAnimationFrame(moveTop);
+});
 
 $('.move_top').on('click', function(e) {
   e.preventDefault();
