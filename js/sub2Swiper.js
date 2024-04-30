5b0c3b9fdbd77f7f540e1e01916aebf9e38b849e
pingandthepong <pingandthepong@gmail.com>
2024-04-30 14:38:22 +0900

    sub2 1,2,3,4 .intro3 마크업 및 스타일링

--- js/sub2Swiper.js
+++ js/sub2Swiper.js
@@ -6,3 +6,11 @@ var swiper1 = new Swiper(".swiper1", {
 var swiper2 = new Swiper('.swiper2', {
   slidesPerView: 2.3,
 }); 
+
+var swiper3 = new Swiper('.swiper3', {
+  effect: "cards",
+  grabCursor: true,
+  // slidesPerView: 1.2,
+  // centeredSlide: true,
+  // spaceBetween: 20,
+})
