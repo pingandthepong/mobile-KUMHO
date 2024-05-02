a42ce022a8034759dc22e61ee4691e74b26156e6
pingandthepong <pingandthepong@gmail.com>
2024-05-02 15:32:32 +0900

    sub4_2 .environment_policy moveList.js 수정

--- js/moveList.js
+++ js/moveList.js
@@ -1,29 +1,25 @@
-const moveSize = 4;
-let position =0;
-let timeOnOff;
+const moveSize = 1;
+let position = 0;
+let ulWidth = $('.move_list_wrap ul').width();
 
-var ulWidth = $('.move_list_wrap ul').width(); // 1200
-
-$('.move_list_wrap ul').after($('.move_list_wrap ul').clone());
+function init() {
+  $('.move_list_wrap ul').after($('.move_list_wrap ul').clone());
+  animate();
+}
 
 function moveList() {
   position -= moveSize;
-  $('.move_list_wrap').css({left: position});
+  $('.move_list_wrap').css({ left: position });
 
-  if (position < -ulWidth) {
-  $('.move_list_wrap').css({left: 0});
-  position = 0;
+  if (position <= -ulWidth) {
+    $('.move_list_wrap').css({ left: 0 });
+    position = 0;
   }
 }
 
-timeOnOff = setInterval(moveList, 100);
+function animate() {
+  moveList();
+  requestAnimationFrame(animate);
+}
 
-
-$('.move_list_wrap').hover(
-  function() {
-    clearInterval(timeOnOff);
-  },
-  function() {
-    timeOnOff = setInterval(moveList, 100);
-  }
-)
+init();
