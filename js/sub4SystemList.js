46227cfe8efa6d6c4a5d40716d3543fea9634c5c
pingandthepong <pingandthepong@gmail.com>
2024-05-02 10:37:06 +0900

    sub4_1 tab2 AOS 삽입 전

---
+++ js/sub4SystemList.js
@@ -0,0 +1,11 @@
+// const systemList = document.querySelectorAll('.system_list li');
+// console.log(systemList);
+
+// // querySelectorAll은 NodeList를 반환하는 '유사배열'이기 때문에 setAttribute 직접 사용 불가
+// // systemList.setAttribute('data-aos', 'fade-left');
+
+// systemList.forEach(function(dlElement) {
+//   dlElement.setAttribute('data-aos', 'fade-left');
+// })
+
+// AOS.refresh();
