1a87f4939dcc6de589ce9433cf1503dcd89ed551
pingandthepong <pingandthepong@gmail.com>
2024-05-02 16:56:31 +0900

    sub4_2 swiper-bundle.min.css & js

--- js/moveList.js
+++ js/moveList.js
@@ -1,25 +1,23 @@
-const moveSize = 1;
+const moveSize = 2;
 let position = 0;
-let ulWidth = $('.move_list_wrap ul').width();
+let ulWidth = $('.move_list_wrap ul').width(); 
+let one = ulWidth/5;
+let total = ulWidth*2;
 
-function init() {
-  $('.move_list_wrap ul').after($('.move_list_wrap ul').clone());
-  animate();
-}
+
+$('.move_list_wrap ul').after($('.move_list_wrap ul').clone());
+
 
 function moveList() {
   position -= moveSize;
   $('.move_list_wrap').css({ left: position });
 
-  if (position <= -ulWidth) {
-    $('.move_list_wrap').css({ left: 0 });
-    position = 0;
+  if (position <= -(ulWidth+one)) {
+    $('.move_list_wrap').css({ left: -one });
+    position = -one;
   }
 }
 
-function animate() {
-  moveList();
-  requestAnimationFrame(animate);
-}
+setInterval(moveList,50);
 
-init();
+
