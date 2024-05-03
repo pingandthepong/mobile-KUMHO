f64c9f93990e171ef4450fa04d04b6a715ee8d45
pingandthepong <pingandthepong@gmail.com>
2024-05-03 12:38:57 +0900

    sub5_1 tabs 마크업 수정 및 스타일링, toggleClass 사용

---
+++ js/sub5TabMenu.js
@@ -0,0 +1,27 @@
+// 초기 상태
+// $('.tab_menu:eq(0) .tab').addClass('current');
+// $('.contlist:eq(0)').show();
+
+// // 클릭 이벤트 핸들러
+// $('.tab').on('click focus', function(e) {
+//   e.preventDefault();
+  
+//   $('.tab_menu .tab').removeClass('current');
+//   $(this).addClass('current');
+  
+//   let ind = $(this).index('.tab');
+//   $('.contlist').hide();
+//   $(`.contlist:eq(${ind})`).show();
+// });
+
+
+$('.target').on('click', function(e) {
+  e.preventDefault();
+
+  // $(this).addClass('on');
+  // $('.tabs').slideDown();
+
+  $(this).toggleClass('on');
+  $('.tabs').slideToggle('fast');
+})
+
